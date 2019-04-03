0a8ea8e86bfc3057cfc480bf0cda2ca8ce9e4cfd
jmkoch <jordan.marie.koch@gmail.com>
2019-04-03 14:36:42 -0500

    fixed indentation error & added import forms statement

--- Traitable/resources.py
+++ Traitable/resources.py
@@ -3,26 +3,25 @@ from .models import Trait, Pub
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources
 from import_export.fields import Field
-#from .forms import TraitForm
+from .forms import TraitForm
 
 class TraitResource(resources.ModelResource):
-	full_trait = Field()
+    full_trait = Field()
 
-	class Meta:
-		model = Trait
-		skip_unchanged = True
-		report_skipped = False
-		fields = ['id', 'genus', 'species', 'ISI', 'fruit_type',]
-		export_order = ['id', 'genus', 'species', 'ISI', 'fruit_type',]
+    class Meta:
+        model = Trait
+        skip_unchanged = True
+        report_skipped = False
+        fields = ['id', 'genus', 'species', 'ISI', 'fruit_type',]
+        export_order = ['id', 'genus', 'species', 'ISI', 'fruit_type',]
 
-	def dehydrate_full_title(self, Trait):
-		return '%s genus %s species' (Trait.genus, Trait.species)
+    def dehydrate_full_title(self, Trait):
+        return '%s genus %s species' (Trait.genus, Trait.species)
 
 # before importing csv, this checks for a blank 'id' col. This adds 'id' col if not present
     def before_import(self, dataset, using_transactions, dry_run=True, collect_failed_rows=False, **kwargs): #raise_errors=True
         if 'id' not in dataset.headers:
             dataset.insert_col(0, lambda row: "", header='id')
-        
         print('Here are the columns you will import:' )
         print(dataset.headers)
 
