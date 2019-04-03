952f2c89b0abe059b1c1be1238ada4031958f421
jmkoch <jordan.marie.koch@gmail.com>
2019-04-03 14:06:31 -0500

    fixed TraitResource class definition & dehydrate_full_title function

--- Traitable/resources.py
+++ Traitable/resources.py
@@ -2,30 +2,21 @@ from django.contrib import admin
 from .models import Trait, Pub
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources
+from import_export.fields import Field
 #from .forms import TraitForm
-#from .resources import TraitResource
-
-class TraitInline(admin.TabularInline):
-	model = Trait
-
-class PubAdmin(admin.ModelAdmin):
-	inlines = [
-		TraitInline,
-	]
-
-admin.site.register(Trait)
-admin.site.register(Pub, PubAdmin)
 
 class TraitResource(resources.ModelResource):
+	full_trait = Field()
 
 	class Meta:
 		model = Trait
 		skip_unchanged = True
 		report_skipped = False
-		fields = ('id', 'genus', 'species', 'ISI', 'fruit_type',)
+		fields = ['id', 'genus', 'species', 'ISI', 'fruit_type',]
+		export_order = ['id', 'genus', 'species', 'ISI', 'fruit_type',]
 
-	def dehydrate_full_title(self, trait):
-		return '%s by %s' (trait.genus, trait.species)
+	def dehydrate_full_title(self, Trait):
+		return '%s genus %s species' (Trait.genus, Trait.species)
 
 #admin.site.register(TraitResource)
 
