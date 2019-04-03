f79ce21c83851effd74c28c9544ff1deb1571c11
jmkoch <jordan.marie.koch@gmail.com>
2019-04-03 14:56:00 -0500

    added urls.py file within Traitable app for traits view for django_import_export

--- Traitable/urls.py
+++ Traitable/urls.py
@@ -1,8 +1,8 @@
+from django.urls import path
 from django.conf.urls import url
-from traits.views import TraitCreateView, export_traits_csv
+from .views import TraitCreateView, export_traits_csv
 
 urlpatterns = [
     url(r'^trait/create/$', TraitCreateView.as_view(success_url = 'successful_upload.html'), name="create trait"),
-    url(r'^export/csv/$', export_traits_csv, name='export_traits_csv'),
-
+    url(r'^export/traits/csv/$', export_traits_csv, name='export_traits_csv'),
 ]
