061cb98647177f8bbc2cb26a327b2da06df3dee5
jmkoch <jordan.marie.koch@gmail.com>
2019-04-03 14:45:27 -0500

    added export_traits_csv function

--- Traitable/views.py
+++ Traitable/views.py
@@ -12,3 +12,17 @@ import csv
 class TraitCreateView(CreateView):
 	model = Trait
 	fields = ('id', 'genus', 'species', 'isi', 'fruit_type')
+
+def export_traits_csv(request):
+	response = HttpResponse(content_type = 'text/csv')
+	response['Content-Disposition'] = 'attachment; filename="traits_output.csv"'
+
+	writer = csv.writer(response)
+	writer.writerow(['id', 'genus', 'species', 'isi', 'fruit_type'])
+
+	traits = Trait.objects.all().values_list('id', 'genus', 'species', 'isi', 'fruit_type')
+
+	for trait in traits:
+		writer.writerow(trait)
+
+	return response
