18c39e5be85d51c624a4f747c3852400243490a1
jmkoch <jordan.marie.koch@gmail.com>
2019-04-04 10:41:29 -0500

    added PubForm for django-import-export

--- Traitable/forms.py
+++ Traitable/forms.py
@@ -13,3 +13,15 @@ class TraitForm(forms.ModelForm):
         if isi > 1.0:
             raise forms.ValidationError("ISI must be between 0.0 and 1.0")
         return self.cleaned_data
+
+class PubForm(forms.ModelForm):
+	class Meta:
+		model = Pub
+		exclude = [id, ]
+	def clean(self):
+		title = self.cleaned_data.get('title')
+		lastName = self.cleaned_data.get('lastName')
+		middleName = self.cleaned_data.get('middleName')
+		firstName = self.cleaned_data.get('firstName')
+		citekey = self.cleaned_data.get('citekey')
+		pub_type = self.cleaned_data.get('pub_type')
