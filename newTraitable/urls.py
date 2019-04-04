960fb4ade2b24b80868228e8de77e4873123ca3d
jmkoch <jordan.marie.koch@gmail.com>
2019-04-04 14:55:33 -0500

    added views import statement but that wasn't necessary

--- newTraitable/urls.py
+++ newTraitable/urls.py
@@ -2,7 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from Traitable import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('', views.)
 ]
