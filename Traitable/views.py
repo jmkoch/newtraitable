5c1c24662cfdea40b7334cbe7d4fda6dd83faf3f
jmkoch <jordan.marie.koch@gmail.com>
2019-04-08 14:26:45 -0500

    fixing preexisting 'simple_upload' function that had residual 'person_data' instead of trait_data

--- Traitable/views.py
+++ Traitable/views.py
@@ -25,14 +25,14 @@ from tablib import Dataset
 
 def simple_upload(request):
     if request.method == 'POST':
-        person_resource = PersonResource()
+        trait_resource = TraitResource()
         dataset = Dataset()
-        new_persons = request.FILES['myfile']
+        new_traits = request.FILES['myfile']
 
-        imported_data = dataset.load(new_persons.read())
-        result = person_resource.import_data(dataset, dry_run=True)  # Test the data import
+        imported_data = dataset.load(new_traits.read())
+        result = trait_resource.import_data(dataset, dry_run=True)  # Test the data import
 
         if not result.has_errors():
-            person_resource.import_data(dataset, dry_run=False)  # Actually import now
+            trait_resource.import_data(dataset, dry_run=False)  # Actually import now
 
     return render(request, 'core/simple_upload.html')
