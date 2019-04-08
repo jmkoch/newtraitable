d89de1612a5d033cf694b0984de9feb162f07c8e
jmkoch <jordan.marie.koch@gmail.com>
2019-04-08 11:28:29 -0500

    removed unnecessary comment

--- Traitable/views.py
+++ Traitable/views.py
@@ -19,3 +19,20 @@ class PubCreateView(CreateView):
 class TraitCreateView(CreateView):
 	model = Trait
 	fields = ('id', 'genus', 'species', 'isi', 'fruit_type')
+
+
+from tablib import Dataset
+
+def simple_upload(request):
+    if request.method == 'POST':
+        person_resource = PersonResource()
+        dataset = Dataset()
+        new_persons = request.FILES['myfile']
+
+        imported_data = dataset.load(new_persons.read())
+        result = person_resource.import_data(dataset, dry_run=True)  # Test the data import
+
+        if not result.has_errors():
+            person_resource.import_data(dataset, dry_run=False)  # Actually import now
+
+    return render(request, 'core/simple_upload.html')
